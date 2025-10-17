1293a5607f28da73bd110dde5f3812314eedaff8
Chayil Walker <100542098+Chayil22@users.noreply.github.com>
2025-10-17 01:43:19 -0500

    Add X / O functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,20 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const board = document.getElementById("board");
-  const squares = Array.from(board.querySelectorAll("#board > div"));
+    const board = document.getElementById("board");
+    const squares = Array.from(board.querySelectorAll("#board > div"));
 
-  //base styling class for each cell
-  squares.forEach(sq => sq.classList.add("square"));
+    squares.forEach(sq => sq.classList.add("square"));
+
+    let currentPlayer = "X";
+    let state = Array(9).fill("");
+
+    //base styling class for each cell
+    squares.forEach((sq, idx) => {
+        sq.addEventListener("click", () => {
+            if (state[idx]) return;              
+            state[idx] = currentPlayer;
+            sq.textContent = currentPlayer;
+            sq.classList.add(currentPlayer);     
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+    });
+  });
 });
