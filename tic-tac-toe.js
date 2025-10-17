a325ef165240faae0551d65eb112676e3d8713be
Chayil Walker <100542098+Chayil22@users.noreply.github.com>
2025-10-17 02:04:25 -0500

    Add New Game Reset

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,16 +2,20 @@ window.addEventListener("DOMContentLoaded", () => {
     const status = document.getElementById("status");
     const board = document.getElementById("board");
     const squares = Array.from(board.querySelectorAll("#board > div"));
+    const newGameBtn = document.querySelector(".btn");
 
     squares.forEach(sq => sq.classList.add("square"));
+
+    const defaultStatus =
+        "Move your mouse over a square and click to play an X or an O.";
 
     let currentPlayer = "X";
     let state = Array(9).fill("");
 
     const wins = [
-        [0,1,2],[3,4,5],[6,7,8],   // rows
-        [0,3,6],[1,4,7],[2,5,8],   // colums
-        [0,4,8],[2,4,6]            // diagonals
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
     ];
 
     function checkWinner() {
@@ -23,8 +27,9 @@ window.addEventListener("DOMContentLoaded", () => {
         return null;
     }
 
-    //base styling clas for each cell
+    //base styling class for each cell
     squares.forEach((sq, idx) => {
+
         //hover visuals for empty cells
         sq.addEventListener("mouseenter", () => {
             if (!state[idx]) sq.classList.add("hover");
@@ -48,6 +53,20 @@ window.addEventListener("DOMContentLoaded", () => {
             } else {
                 currentPlayer = currentPlayer === "X" ? "O" : "X";
             }
+        });
+    });
+
+    // Board reset
+    newGameBtn.addEventListener("click", () => {
+        state = Array(9).fill("");
+        currentPlayer = "X";
+        status.textContent = defaultStatus;
+        status.classList.remove("you-won");
+
+        squares.forEach(sq => {
+            sq.textContent = "";
+            sq.classList.remove("X", "O", "hover");
+            if (!sq.classList.contains("square")) sq.classList.add("square");
         });
     });
 });
